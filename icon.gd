dddc2feb19d96d773e7c314133924920e71e018f
NeilRevil03 <revilneilangelo@gmail.com>
2025-04-14 16:58:40 +0800

    made a game

--- icon.gd
+++ icon.gd
@@ -4,12 +4,15 @@ var coins = 5
 var player_name = "robot"
 var hearts = 3.5
 const speed = 2
-var
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	print("hello world")
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	rotate_y(deg_to_rad(5))
+	
 	pass
